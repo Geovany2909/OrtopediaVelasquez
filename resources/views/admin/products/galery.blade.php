1539cc836396111beefd04ac493a811add664042
Geovany2909 <geovanimarroquin114@gmail.com>
2020-07-30 22:07:00 -0600

    menu

--- resources/views/admin/products/galery.blade.php
+++ resources/views/admin/products/galery.blade.php
@@ -15,25 +15,24 @@ Galery
 <main>
     @section('buscar')
     {{ Form::open(['route' => 'galery', 'method' => 'GET', 'class' => 'pone-la-clase-que-queras-para-el-form']) }}
-    <i class="fa fa-search" aria-hidden="true"></i>
     <input type="text" name="name" placeholder="Buscar por nombre">
+    <i class="fa fa-search" aria-hidden="true"></i>
     <button type="submit" style="display: none"></button>
     {{ Form::close() }}
     @endsection
     <div class="main-menu">
         @forelse ($products as $product)
-        {{-- <a href="{{ route('showOnlyProduct', $product->id) }}"> --}}
-            <a href="javascript:void(0);" onclick="fun_galeryAdmin('{{ $product->id }}')">
-            <div class="option">
-                <img class="imagen" src="/images/products/{{ $product->photo ? $product->photo : 'ortesis.jpg' }}"
-                    height="100" width="150" alt="">
-                <h2>{{ $product->name }}</h2>
-            </div>
-        </a>
+            <a href="javascript:void(0);" class="cards" onclick="fun_galeryAdmin('{{ $product->id }}')">
+                <div class="option">
+                    <img class="imagen" src="/images/products/{{ $product->photo ? $product->photo : 'ortesis.jpg' }}"
+                        height="100" width="150" alt="">
+                    <h2>{{ $product->name }}</h2>
+                </div>
+            </a>
         @empty
-        @section('empty')
-        <h2 style="color: red; text-align: center;">En este momento no hay productos</h2>
-        @endsection
+            @section('empty')
+                <h2 style="color: red; text-align: center;margin-right: 20%;">En este momento no hay productos</h2>
+            @endsection
         @endforelse
     </div>
     {{ $products->links() }}
